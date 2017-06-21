ea824b005c631ce24f138fa13ecb4555d846b8cf
nonakinha <nonaka04@hotmail.co.jp>
2017-06-22 08:51:55 +0900

    Initial Commit

---
+++ C/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C
+//
+//  Created by 野中明 on 2017/06/22.
+//  Copyright © 2017年 mycompany. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
